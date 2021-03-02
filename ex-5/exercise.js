02a777581ec0ed394b726795fde9afe2081133c8
Rawah G <rawah.h@outlook.com>
2021-03-02 17:51:50 +0100

    updated

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -3,7 +3,21 @@ function loadBooks(array) {
 }
 
 // Paste here your array
-
-
+var books = [
+    { title: 'The Design of EveryDay Things',  author: 'Don Norman', alreadyRead: false }, 
+    { title: 'The power of now', author: 'Ekhart Tolle', alreadyRead: true }, 
+    { title: 'The Three Body Problem', author: 'Liu Cixin', alreadyRead: true }
+  ];
+ 
+  books.forEach(function(item){
+let title = document.createElement("p");
+let author = document.createElement("p");
+let title = document.createTextNode(item.title);
+let author = document.createTextNode(item.author);
+titleTag.appendChild(title);
+titleTag.appendChild(author);
+document.getElementsByTagName("body")[0].appendChild(titleTag);
+document.getElementsByTagName("body")[0].appendChild(author);
+})
 // This function will execute when the window loads
 window.onload = loadBooks(array);
