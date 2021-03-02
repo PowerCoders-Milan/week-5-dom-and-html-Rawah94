dcd25e492d83224bda2e043abd5f94460de64faf
Rawah G <rawah.h@outlook.com>
2021-03-02 12:21:01 +0100

    updated

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,9 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
-
+const paragraphs = document.getElementsByTagName("p");
+paragraphs[0].style.backgroundColor = "black"; 
+paragraphs[0].style.color = "white"; 
+paragraphs[1].style.backgroundColor = "blue";
+paragraphs[2].style.backgroundColor = "grey";
 }
